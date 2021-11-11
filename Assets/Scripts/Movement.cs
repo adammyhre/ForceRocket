9889b06537dc732783b14c4937395c12bd0dee2a
Adam Myhre <adam@undergalaxie.com>
2021-11-10 18:32:49 -0800

    First commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] float rotationThrust =  50f;
+    Rigidbody rb;
+
+    void Awake() {
+        rb =  GetComponent<Rigidbody>();
+    }
+    
+    void Update() {
+    }
+
+    void FixedUpdate() {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust() {
+        if (Input.GetKey(KeyCode.Space)) {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation() {
+        if (Input.GetKey(KeyCode.A)) {
+            ApplyRotation(rotationThrust);
+        } 
+        else if (Input.GetKey(KeyCode.D)) {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    void ApplyRotation(float rotation) {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotation * Time.deltaTime);
+        rb.freezeRotation = false;
+    }
+}
