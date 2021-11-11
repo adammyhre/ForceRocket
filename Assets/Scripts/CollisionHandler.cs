43bdf2b9b4ed3389dc1281f0f2b2edeaa0219045
Adam Myhre <adam@undergalaxie.com>
2021-11-10 19:50:55 -0800

    WIP

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour {
+
+    [SerializeField] float levelLoadDelay = 2f;
+
+    void OnCollisionEnter(Collision other) {
+        switch(other.gameObject.tag) {
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            case "Friendly":
+                print("Friendly");
+                break;
+            default: 
+                StartCrashSequence();
+                break;
+        }
+    }
+
+    void StartCrashSequence() {
+        GetComponent<Movement>().enabled = false;
+        StartCoroutine(ReloadScene());
+    }
+
+    void StartSuccessSequence() {
+        GetComponent<Movement>().enabled = false;
+        StartCoroutine(LoadNextLevel());
+
+    }
+
+    IEnumerator LoadNextLevel() {
+        yield return new WaitForSeconds(levelLoadDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = 
+            (currentSceneIndex == SceneManager.sceneCountInBuildSettings)
+            ? 0
+            : currentSceneIndex + 1;
+        
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    IEnumerator ReloadScene() {
+        yield return new WaitForSeconds(levelLoadDelay);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,9 +7,11 @@ public class Movement : MonoBehaviour {
     [SerializeField] float mainThrust = 1000f;
     [SerializeField] float rotationThrust =  50f;
     Rigidbody rb;
+    AudioSource audioSource;
 
     void Awake() {
         rb =  GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
     
     void Update() {
@@ -23,6 +25,12 @@ public class Movement : MonoBehaviour {
     void ProcessThrust() {
         if (Input.GetKey(KeyCode.Space)) {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+
+            if (!audioSource.isPlaying) {
+                audioSource.Play();
+            }
+        } else {
+            audioSource.Stop();
         }
     }
 
